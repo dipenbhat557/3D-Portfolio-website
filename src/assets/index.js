de90d0969421f77ea6f06ffa47a2071de8d6d417
dipenbhat557 <dipenbhat557@gmail.com>
2024-07-25 15:27:38 +0530

    updated tech stack and d project data

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,24 +7,15 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import springBoot from "./tech/springBoot.jpg";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import reactjs from "./tech/reactjs.png";
 import tailwind from "./tech/tailwind.png";
-import jsp from "./tech/jsp.jpg";
 import mysql from "./tech/mysql.jpg";
-import thymeleaf from "./tech/thymeleaf.jpg";
 import postgresql from "./tech/postgresql.png";
-import microservices from "./tech/microservices.jpg";
-import hibernate from "./tech/hibernate.jpg";
-import aws from "./tech/aws.png";
-import digitalocean from "./tech/digitalocean.jpg";
 
-import foss from "./company/foss.jpg";
 import gdsc from "./company/gdsc.jpg";
 
 import contactManager from "./contactManager.jpg";
@@ -40,23 +31,14 @@ export {
   github,
   menu,
   close,
-  css,
   docker,
   git,
-  html,
   javascript,
   reactjs,
   tailwind,
   springBoot,
-  jsp,
   mysql,
-  thymeleaf,
   postgresql,
-  microservices,
-  hibernate,
-  aws,
-  digitalocean,
-  foss,
   gdsc,
   contactManager,
   techBlog,
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,22 +2,13 @@ import {
   backend,
   web,
   javascript,
-  html,
-  css,
   reactjs,
   tailwind,
   git,
   docker,
   springBoot,
-  jsp,
   mysql,
-  thymeleaf,
   postgresql,
-  microservices,
-  hibernate,
-  aws,
-  digitalocean,
-  foss,
   gdsc,
   techBlog,
   contactManager,
@@ -41,27 +32,39 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Frontend Developer",
     icon: web,
-  },
-  {
-    title: "Spring Boot Developer",
-    icon: springBoot,
   },
   {
     title: "Backend Developer",
     icon: backend,
   },
+  {
+    title: "Cloud Services",
+    icon: web,
+  },
 ];
 
 const technologies = [
   {
-    name: "JavaScript",
-    icon: javascript,
+    name: "Spring Boot",
+    icon: springBoot,
+  },
+  {
+    name: "Next JS",
+    icon: nextJs,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
   },
   {
     name: "React JS",
     icon: reactjs,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
   },
   {
     name: "Tailwind CSS",
@@ -76,12 +79,24 @@ const technologies = [
     icon: docker,
   },
   {
-    name: "spring boot",
-    icon: springBoot,
+    name: "kubernetes",
+    icon: kubernetes,
   },
   {
-    name: "jsp",
-    icon: jsp,
+    name: "jenkins",
+    icon: jenkins
+  },
+  {
+    name: "firebase",
+    icon: firebase
+  },
+  {
+    name: "kafka",
+    icon: kafka
+  },
+  {
+    name: "wordpress cms",
+    icon: wordpress
   },
   {
     name: "mysql",
@@ -91,80 +106,223 @@ const technologies = [
     name: "postgresql",
     icon: postgresql,
   },
-  {
-    name: "microservices",
-    icon: microservices,
-  },
-  {
-    name: "hibernate",
-    icon: hibernate,
-  },
-  {
-    name: "aws",
-    icon: aws,
-  },
-  {
-    name: "digitalocean",
-    icon: digitalocean,
-  },
 ];
 
 const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "SajiloDev",
+    icon: sajilodev,
+    iconBg: "#E6DEDD",
+    date: "November 2023 - June 2024",
+    points: [
+      "Developerd and deployed multiple production ready projects including official website of sajilodev",
+      "Implementing best practices for software development and version control.",
+      "Troubleshooting and resolving technical issues in collaboration with the community.",
+      "Promoting open-source initiatives through advocacy and educational events.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Geoland Tours and Travels",
+    icon: geoland,
+    iconBg: "#383E56",
+    date: "January 2024 - April 2024",
+    points: [
+      "Created different consoles for user and admin using React typescript and tailwind in frontend and firebase in backend",
+      "Optimizing database queries and improving server performance.",
+      "Integrating third-party APIs to enhance application functionality.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Deukhuri Multiple Campus",
+    icon: deukhuri,
+    iconBg: "#E6DEDD",
+    date: "December 2023 - August 2024",
+    points: [
+      "Created different consoles for user and admin using React typescript and tailwind in frontend and backend in spring boot",
+      "Optimizing database queries and improving server performance.",
+      "Integrating third-party APIs to enhance application functionality.",
+    ],
+  },
   {
     title: "Backend Facilitator",
     company_name: "Google DSC FETJU",
     icon: gdsc,
     iconBg: "#383E56",
-    date: "July 2023 - Present",
+    date: "July 2023 - July 2024",
     points: [
       "Leading backend development efforts for web applications using Spring Boot.",
       "Optimizing database queries and improving server performance.",
       "Integrating third-party APIs to enhance application functionality.",
     ],
   },
-  {
-    title: "Technical Officer",
-    company_name: "FOSS",
-    icon: foss,
-    iconBg: "#E6DEDD",
-    date: "September 2023 - Present",
-    points: [
-      "Managing technical aspects of open-source projects and contributing code.",
-      "Implementing best practices for software development and version control.",
-      "Troubleshooting and resolving technical issues in collaboration with the community.",
-      "Promoting open-source initiatives through advocacy and educational events.",
-    ],
-  },
+  
 ];
 
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "Working with sajiloDev was a game-changer for my online presence. They took the time to understand my goals and delivered a website that not only looks fantastic but also performs exceptionally well. Their expertise and dedication are unmatched!",
+    name: "Govinda Sharma",
+    designation: "Managing Director",
+    company: "Geoland Travels",
+    image: govinda,
+  },
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Dipendra proved me wrong.",
+    name: "Shiv Raj Khanal",
+    designation: "Lamahi Chamber of Commerce",
+    company: "Founder",
+    image: shivraj,
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    name: "Shiva Oli",
+    designation: "Campus Chief",
+    company: "Deukhuri Multiple Campus",
+    image: shivaoli,
   },
   {
     testimonial:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
+    name: "Tek Bahadhur Pun ",
     designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    company: "GharSanchar",
+    image: tekpun,
   },
 ];
 
 const projects = [
+  {
+    name: "SajiloDev",
+    description:
+      "A web platform built with React.ts and Firebase for ordering technical services such as website creation, domain checks, hosting, and maintenance. It provides a user-friendly interface for managing these services.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "yellow-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "white-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+    ],
+    image: sajilodev,
+    source_code_link: "https://github.com/dipenbhat557/SajiloDev",
+    website_link: "https://sajilodev.com"
+  
+  },
+  {
+    name: "Payment Management System",
+    description:
+      "A Next.js and TypeScript-based platform that connects multiple bank accounts, displays real-time transactions, and allows fund transfers. It integrates with Plaid for account linking and Dwolla for transfers, featuring secure authentication and responsive design."    tags: [
+      {
+        name: "Next JS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Plaid",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Dwolla",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Appwrite",
+        color: "yellow-text-gradient",
+      },
+      {
+        name: "Sentry",
+        color: "pink-text-gradient",
+      },
+
+    ],
+    image: paymentManagement,
+    source_code_link: "https://github.com/dipenbhat557/payment-management",
+  },
+  {
+    name: "Geoland Travels",
+    description:
+      "A travel platform with admin and user panels, built with React.js and TypeScript, and powered by Firebase for the backend. It offers inbound and outbound tours, ticketing services, and pricing details.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "white-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "green-text-gradient",
+      },
+    ],
+    image: geoland,
+    source_code_link: "https://github.com/dipenbhat557/Geoland-Travels",
+    website_link: "https://geolandtravels.com"
+  },
+  {
+    name: "Deukhuri Multiple Campus",
+    description:
+      "A learning management system with admin and user panels, built with React.js and TypeScript, and powered by Spring Boot for the backend with mySQL for database. It offers insights into various notices, publications, college team and also handles the admission process.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "white-text-gradient",
+      },
+      {
+        name: "spring boot",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mysql",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: deukhuri,
+    source_code_link: "https://github.com/dipenbhat557/Deukhuri-College",
+    website_link: "https://deukhurimultiplecampus.edu.np"
+  },
+  {
+    name: "Ghar Sanchar",
+    description:
+      "A news website in Nepal, featuring simple UI design for the news in nepal.",
+    tags: [
+      {
+        name: "wordpress",
+        color: "blue-text-gradient",
+      },
+      
+    ],
+    image: gharsanchar,
+    website_link: "https://gharsanchar.com"
+  },
   {
     name: "Whatsapp Clone",
     description:
